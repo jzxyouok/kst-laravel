2c639f610654f36f4f57bc544d6478f7e07b3484
hkk710 <hkk710@gmail.com>
2016-09-16 16:32:33 +0530

    mainpage updated

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -25,56 +25,56 @@
 	</head>
 	<body>
 		<div id="nav-main">
-			<div id="n-m-img">
+			<div id="n-m-img ad-mg-btm-0">
 				<img src="{{asset('images/hkk.jpg')}}" width="100%" height="400">
 			</div>
-			<nav class="navbar navbar-inverse n-m-nav">
+		</div>
+		<div class="nav-width">
+			<nav class="navbar navbar-inverse n-m-nav ad-mg-btm-0">
 				<div class="container-fluid">
-			    	<div class="navbar-header">
-			    		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-					        <span class="icon-bar"></span>
-					        <span class="icon-bar"></span>
-					        <span class="icon-bar"></span>                        
-			      		</button>
-			      		<a class="navbar-brand" href="#">KESAVAPURAM SREEKRISHNASWAMY TEMPLE</a>
-			    	</div>
-			    	<div class="collapse navbar-collapse" id="myNavbar">
-			      		<ul class="nav navbar-nav">
-			        		<li class="active"><a href="#">Home</a></li>
-			        		<li><a href="#">Page 1</a></li>
-			        		<li><a href="#">Page 2</a></li>
-			        		<li><a href="#">Page 3</a></li>
-			      		</ul>
-			      		<ul class="nav navbar-nav navbar-right">
-			      			@if (Auth::guest())
-						        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-						        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-						    @else 
-						    	<li class="dropdown">
-		                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-		                                {{ Auth::user()->name }} <span class="caret"></span>
-		                            </a>
-
-		                            <ul class="dropdown-menu" role="menu">
-		                                <li><a href="{{url('/home')}}"><i class="fa fa-user" aria-hidden="true"></i> Profile</a></li>
-		                                <div class="divider"></div>
-		                                <li>
-		                                    <a href="{{ url('/logout') }}"
-		                                        onclick="event.preventDefault();
-		                                                 document.getElementById('logout-form').submit();"> <i class="fa fa-sign-out" aria-hidden="true"></i>  Logout
-		                                    </a>
-
-		                                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-		                                        {{ csrf_field() }}
-		                                    </form>
-		                                </li>
-		                            </ul>
-		                        </li>
-						    @endif
-			      		</ul>
-			    	</div>
-			  	</div>
+					<ul class="nav navbar-nav">
+						<li><a href="#"><b>Home</b></a></li>
+						<li><a href="#">About</a></li>
+				        <li><a href="#">Contact us</a></li>
+				        <li><a href="#">Upadevas</a></li>
+				        <li><a href="#">Organisational setup</a></li>
+				        <li><a href="#">Activities</a></li>
+				        <li><a href="#">Festivals</a></li>
+				        <li><a href="#">Facilities</a></li>
+					</ul>	
+			    </div>
 			</nav>
+		
+
+			<div id="n-m-body" style="margin-top: 15px;">
+				<div class="col-md-3 ad-bd">
+					<div class="n-m-b-heading">
+						<b>Dashboard</b>
+					</div>
+					<div class="ad-pd">
+						<ul class="templatemo-submenu">
+			                <li><a href="#">
+			                  <i class='fa fa-eye' style='color:#F14700'></i> Gallery </a></li>
+			                <li> <a href="#">
+			                    <i class='fa fa-eye' style='color:#F14700'></i> Daily Rituals </a></li>
+			                <li><a href="#">
+			                    <i class='fa fa-eye' style='color:#F14700'></i> Offerings </a></li>
+			                <li><a href="#">
+			                    <i class='fa fa-eye' style='color:#F14700'></i> Online Vazhipad </a></li>
+			                <li><a href="#">
+			                    <i class='fa fa-eye' style='color:#F14700'></i> Pooja Details </a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-9 ad-bd at-media-1">
+					<div class="n-m-b-heading">
+						
+					</div>
+					<div class="ad-pd">
+						
+					</div>
+				</div>
+			</div>
 		</div>
 	</body>
 </html>
