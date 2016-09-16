726f3d762908d1e87ee59926fd169de3742e7a6c
hkk710 <hkk710@gmail.com>
2016-09-17 01:59:44 +0530

    home page updated

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -34,7 +34,6 @@
 				<div class="container-fluid">
 					<ul class="nav navbar-nav">
 						<li><a href="#"><b>Home</b></a></li>
-						<li><a href="#">About</a></li>
 				        <li><a href="#">Contact us</a></li>
 				        <li><a href="#">Upadevas</a></li>
 				        <li><a href="#">Organisational setup</a></li>
@@ -44,8 +43,9 @@
 					</ul>	
 			    </div>
 			</nav>
-		
-
+			<div class="scroll-slow" style="color: #990000;font-size:22px;">
+			<p><b>ഹരേ	രാമ		ഹരേ	രാമ	....രാമ	രാമ		ഹരേ	ഹരേ...........ഹരേ		കൃഷ്ണ	ഹരേ	കൃഷ്ണ.......കൃഷ്ണ കൃഷ്ണ	ഹരേ	ഹരേ...</b>	</p>
+			</div>
 			<div id="n-m-body" style="margin-top: 15px;">
 				<div class="col-md-3 ad-bd">
 					<div class="n-m-b-heading">
@@ -71,10 +71,82 @@
 						
 					</div>
 					<div class="ad-pd">
+					<div class="col-sm-6 col-md-0 col-md-offset-0" style="padding-top: 20px;">
+<img src="{{asset('images/kannan.jpg')}}" class= "img-circle">
+</div>
+		<div  style="height:500px;">
+  			<div class="col-md-6" style="padding-top: 5px;" >
+				<div class="content_white" style="line-height: 1em;color: #4700b3;">
+					<h1> ശ്രീകൃഷ്ണധ്യാനം  </h1>
+					<h4>കസ്തൂരി തിലകം ലലാട ഫലകേ
+                        <br>
+                        വക്ഷസ്ഥലേ കൗസ്തുഭം
+                        <br>
+                        നാസാഗ്രേ നവ  മൗക്തികം കരതലേ
+                        <br>
+                        വേണും കരേ കങ്കണം
+                        <br>
+                        സർവ്വാംഗേ ഹരിചന്ദനം ചകലയൻ
+                        <br>
+                        കണ്ധെ ച മുക്താ വലീം
+                        <br>
+                        ഗോപസ്ത്രീ പരിവേഷ്ട്ടിതോ വിജയതേ
+                        <br>
+                        ഗോപലച്ചുധാമണി
+                        <br>
+                        <br>
+                        സാരം :	നെറ്റിയിൽ കസ്തൂരിതിലകം.
+                        <br>
+                        മാറിൽ കൗസ്തുഭമണി, മൂക്കിൽ പവിഴം
+                        <br>
+                        പതിച്ച നാസാമണി, കരതളിരുകളിൽ
+                        <br>
+                        വേണു, കൈത്തണ്ടയിൽ  കങ്കണം
+                        <br>
+                        ദേഹമാസകലം ചന്ദനം, കഴുത്തിൽ
+                        <br>
+                        മുത്തുമാല, അങ്ങനെ ഗോപാലന്മാർക്കു
+                        <br>
+                        ശിരോലങ്കാരമായ കൃഷ്ണൻ
+                        <br>
+                        ഗോപാംഗനകളാൽ ചുറ്റപ്പെട്ട്  വിജയിച്ചരുളുന്നു.
+                        <br>
+                        </h4>
+		   		</div>
+			</div>
+
+			<div class="col-md-6" >
+				<div class="container-top" >
+	        </div> 
+	                            </div> 
+	                        </div>
+	                    </div> 
+	                </fieldset>
+	            </form>
+	        </div> 
+	            </div>
+	        </div> 
+	    </div> 
+	</div>  
+</div>
+			</div>
+		</div>
+
+		   </div>
+		  </div>
+		</div>
 						
 					</div>
 				</div>
 			</div>
 		</div>
+
+<footer class="site-footer" style="background-color:#3b5998;color: #fff!Important; position: fixed; width:100%; bottom:0;">
+  <div class="container" style="text-align: center;">
+      <p>Copyright © 2016 | All rights reserved | Design by <a href="http://www.hkwebdevelopers.weebly.com">
+     Kesavapuram Brothers</a></p>
+  </div>
+</footer>
+
 	</body>
-</html>
+</html>	
---
+++ resources/views/layouts/prathishtas.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<body>
+    <meta charset="UTF-8">
+    <title>PRATHISHTAS</title>
+   <h3 class=" col-md-offset-2 " align="center">PRATHISHTA & UPAPRATHISHTAS/h3> <br>
+    <div class="row" >
+  <div class="col-xs-6 col-md-3 " >
+    <a href="#" class="thumbnail">
+      <img src="{%  images 'sreekrishna.jpg' %}" alt="...">
+    </a>
+  </div>
+   </div>
+    <div class="row">
+  <div class="col-xs-6 col-md-3 col-md-offset-9">
+    <a href="#" class="thumbnail">
+      <img src="{%  images 'ganesh.jpg' %}" alt="...">
+    </a>
+  </div>
+  ...
+</div>
+    <div class="row">
+  <div class="col-xs-6 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="{%  images 'devi.jpg' %}" alt="...">
+    </a>
+  </div>
+  ...
+</div>
+    <div class="row">
+  <div class="col-xs-6 col-md-3 col-md-offset-9">
+    <a href="#" class="thumbnail">
+      <img src="{%  images 'nagar.jpg' %}" alt="...">
+    </a>
+  </div>
+  ...
+</div>
+</head>
+
+</body>
+</html>
