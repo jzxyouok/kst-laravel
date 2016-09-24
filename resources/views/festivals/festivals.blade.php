6af2cb3c7e622a1c83a9a74a5e135944864ab9b8
hkk710 <hkk710@gmail.com>
2016-09-25 03:20:19 +0530

    arranged

--- resources/views/festivals/festivals.blade.php
+++ resources/views/festivals/festivals.blade.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
     <html>
     <head>
+    <style>
+        .img-responsive {
+           height:32%;
+           width:32%;
+        }
+    </style>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -29,25 +35,25 @@
     <h1 style="color:#fff; text-align: center;">FESTIVALS</h1>
     <ol style="color:#e60000">
     <li>JANMASHTAMI MAHOTSAVAM 6 DAYS</li>
-    <img src="{{asset('images/festival1.jpg')}}" alt="" class="img-thumbnail">
+    <img src="{{asset('images/festival1.jpg')}}" alt="" class="img-responsive">
     <li>PRETHISHTA DHINAM 1 DAY</li>
-    <img src="{{asset('images/p1.jpg')}}" alt="" class="img-thumbnail">
+    <img src="{{asset('images/p1.jpg')}}" alt="" class="img-responsive">
     <li>BHAGAVADA SAPTHAHAM</li>
-    <img src="{{asset('images/sap1.jpg')}}" alt="" class="img-thumbnail">
+    <img src="{{asset('images/sap1.jpg')}}" alt="" class="img-responsive">
     <li>SWARGAVATHIL EKADESHI</li>
-    <img src="{{asset('images/swa1.jpg')}}" alt="" class="img-thumbnail">
+    <img src="{{asset('images/swa1.jpg')}}" alt="" class="img-responsive">
     <li>NAVARATHIRI MAHOTSAVAM</li>
-    <img src="{{asset('images/nav1.jpg')}}" alt="" class="img-thumbnail">
+    <img src="{{asset('images/nav1.jpg')}}" alt="" class="img-responsive">
     <li>MANDALA CHIRAPPU MAHOTSAVAM</li>
-    <img src="{{asset('images/ayappan.jpg')}}" alt="" class="img-thumbnail">
+    <img src="{{asset('images/ayappan.jpg')}}" alt="" class="img-responsive">
     <li>THRIKARTHIKA MAHOTSAVAM</li>
-    <img src="{{asset('images/tk1.jpg')}}" alt="" class="img-thumbnail">
+    <img src="{{asset('images/tk1.jpg')}}" alt="" class="img-responsive">
     <li>VISHU KANNI</li>
-    <img src="{{asset('images/vishu.jpeg')}}" alt="" class="img-thumbnail">
+    <img src="{{asset('images/vishu.jpeg')}}" alt="" class="img-responsive">
     <li>VINAYAKA CHATHURTHI</li>
-    <img src="{{asset('images/gc.jpg')}}" alt="" class="img-thumbnail">
+    <img src="{{asset('images/gc.jpg')}}" alt="" class="img-responsive">
     <li>THIRUVATHIRA MAHOTSAVAM</li>
-    <img src="{{asset('images/thiru.jpg')}}" alt="" class="img-thumbnail">
+    <img src="{{asset('images/thiru.jpg')}}" alt="" class="img-responsive">
     </ol>
     </body>
     </html>
